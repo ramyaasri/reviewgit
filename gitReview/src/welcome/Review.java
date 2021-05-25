59612262925832ff23c76536bdf059946308a72e
ramyaasri <raamyaa.sri@gmail.com>
2021-05-25 16:44:49 +0530

    modifications are done

--- gitReview/src/welcome/Review.java
+++ gitReview/src/welcome/Review.java
@@ -1,8 +1,9 @@
-package hello;
+package welcome;
 
-public class Greeting {
+public class Review {
 	public static void main(String[] args) {
 		System.out.println("Hi Ramya");
+		System.out.println("This is for Review");
 	}
 
 }
