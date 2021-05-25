8a59d5c4b22376ed9952a14e3ce4d96e152d9281
ramyaasri <raamyaa.sri@gmail.com>
2021-05-25 16:41:38 +0530

    pushed to repo

---
+++ gitReview/src/hello/Greeting.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class Greeting {
+	public static void main(String[] args) {
+		System.out.println("Hi Ramya");
+	}
+
+}
---
+++ gitReview/src/welcome/client.java
@@ -0,0 +1,8 @@
+package welcome;
+
+public class client {
+	public static void main(String[] args) {
+		System.out.println("This is for git  Capability");
+	}
+
+}
